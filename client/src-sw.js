10cede9d79c74e2218ce32098f5739b527345fdf
Pain <holtbrenden@yahoo.com>
2022-04-08 16:05:44 -0600

    finished todos still needs work

---
+++ client/src-sw.js
@@ -0,0 +1,44 @@
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+registerRoute(
+  // call back that filters requests
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of cache
+    cacheName: 'asset-cache',
+    plugins: [
+      // cache responses max age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+registerRoute();
---
+++ client/src/js/database.js
@@ -0,0 +1,52 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => { 
+  console.log('put to database')
+
+  // create connection to db
+  const jateDb = await openDB('jate', 1)
+  //new transaction
+  const tx = jateDb.transaction('jate', 'readwrite')
+
+  // open up object store
+  const store = tx.objectStore('jate')
+
+  // adding content
+  const request = store.add({content})
+
+  const result = await request
+  console.log('saved to db')
+}
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => { 
+  // console.error('')
+  console.log('get from db')
+// connect to db
+ const jateDb = await openDB('jate', 1)
+// new transaction
+ const tx = jateDb.transaction('jate', 'readonly')
+
+ // open up object store
+ const store = tx.objectStore('jate')
+
+ // adding content
+ const request = store.add({content})
+
+ const result = await request
+ console.log('result.value', result)
+}
+initdb();
